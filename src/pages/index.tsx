b11764e00f56946fbc6aecccde90090d8a7d641e
DrizzlingCattus <hit0473@gmail.com>
2021-04-22 03:38:18 +0900

    Update: 임시 about 페이지 내용 개선

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,21 +10,25 @@ export default function Index() {
       <BasicMeta url={"/"} />
       <OpenGraphMeta url={"/"} />
       <TwitterCardMeta url={"/"} />
-      <div className="container">
+      <div className="profile">
         <div>
-          <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
-          </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
-          <SocialList />
-        </div>
+          <div>
+            <h1>김윤환</h1>
+            <span className="handle">@DrizzlingCattus</span>
+            <p>일사천리로, 적어도 더듬더듬 올바르게 진행할 수 있기를...</p>
+            <SocialList />
+          </div>
+      	</div>
+	<img className="profile-image" src="https://avatars.githubusercontent.com/u/4661295?s=400&u=f9cc8a837ea4354ffe0c822cdb9632a86a03aa3e&v=4" />
       </div>
       <style jsx>{`
-        .container {
+	.profile-image {
+	  border-radius: 50%;
+	}
+        .profile {
           display: flex;
           align-items: center;
-          justify-content: center;
+          justify-content: space-around;
           flex: 1 1 auto;
           padding: 0 1.5rem;
         }
